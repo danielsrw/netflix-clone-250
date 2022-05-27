81d4be14f3527e9a9c2d2475a7313103592111d1
danielsrw <danielsrwanda@gmail.com>
2022-05-27 03:48:41 +0200

    Initial release

--- pages/index.tsx
+++ pages/index.tsx
@@ -34,7 +34,7 @@ const Home = ({
     if (loading) return null
 
     return (
-        <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
+        <div className={`relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh] ${ showModal && '!h-screen overflow-hidden' }`} >
             <Head>
                 <title>
                     <title>Home - Netflix</title>
